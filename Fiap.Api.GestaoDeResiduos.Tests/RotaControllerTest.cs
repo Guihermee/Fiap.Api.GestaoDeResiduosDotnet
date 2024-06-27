869b168f4de4b234d0f9e6ed481fabc01bcabc8f
Guilherme <125518739+Guihermee@users.noreply.github.com>
2024-06-27 18:19:58 -0300

    Projeto Teste

---
+++ Fiap.Api.GestaoDeResiduos.Tests/RotaControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Fiap.Api.GestaoDeResiduos.Controllers;
+using Fiap.Api.GestaoDeResiduos.Data.Context;
+using Fiap.Api.GestaoDeResiduos.Model;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fiap.Api.GestaoDeResiduos.Tests
+{
+    public class RotaControllerTest
+    {
+
+        private readonly Mock<DatabaseContext> _contextMock;
+        private readonly RotaController _controller;
+        private readonly DbSet<RotaModel> _mockSet;
+
+        public RotaControllerTest()
+        {
+            _contextMock = new Mock<DatabaseContext>();
+            _mockSet = new Mock<DbSet<RotaModel>>().Object;
+            _controller = new RotaController(_contextMock.Object);
+        }
+
+    }
+}
---
+++ Fiap.Api.GestaoDeResiduos.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Fiap.Api.GestaoDeResiduos.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
